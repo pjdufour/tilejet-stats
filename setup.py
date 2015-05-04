28d207f0648ec0de2cfa19456df318dbdf18a49c
pjdufour <pjdufour.dev@gmail.com>
2015-05-04 01:40:32 -0400

    Initial code

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='tilejet-stats',
+    version='0.0.1',
+    install_requires=[],
+    author='TileJet Developers',
+    author_email='XXX',
+    license='MIT License',
+    url='https://github.com/tilejet/tilejet-stats/',
+    keywords='python gis tilejet',
+    description='A python utility library containing functions for generating statistics on tile service usage.',
+    long_description=open('README.md').read(),
+    download_url="https://github.com/tilejet/tilejet-stats/zipball/master",
+    py_modules=["tilejet-stats"],
+    classifiers = [
+        'Development Status :: 1 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
